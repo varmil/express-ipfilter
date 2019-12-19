a44e28e99aaf5000ac734f560aa9105434f13413
varmil <fkmks247@gmail.com>
2019-12-19 15:15:36 +0900

    update type def

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,32 @@
-import express = require('express');
+import express = require('express')
 
-export type Ip              = string;
-export type CidrRange       = string;
-export type Route           = string;
-export type StartEndIpRange = Ip[];
-export type IpRange         = CidrRange | StartEndIpRange;
-export type IpList          = Array<Ip | IpRange>;
+export type Ip = string
+export type CidrRange = string
+export type Route = string
+export type StartEndIpRange = Ip[]
+export type IpRange = CidrRange | StartEndIpRange
+export type IpList = Array<Ip | IpRange>
 
 export interface IpCallback {
-  (): IpList;
+  (req: express.Request): IpList
 }
 
 export interface IpFilterOptions {
-  detectIp?: (request: express.Request) => Ip;
-  excluding?: Route[];
-  log?: boolean;
-  logLevel?: 'all' | 'deny' | 'allow';
-  mode?: 'deny' | 'allow';
+  detectIp?: (request: express.Request) => Ip
+  excluding?: Route[]
+  log?: boolean
+  logLevel?: 'all' | 'deny' | 'allow'
+  mode?: 'deny' | 'allow'
   // `@types/proxy-addr` does not export the `trust` parameter type
-  trustProxy?: any;
+  trustProxy?: any
 }
 
 export function IpFilter(
   ips: IpList | IpCallback,
-  opts?: IpFilterOptions,
-): express.RequestHandler;
+  opts?: IpFilterOptions
+): express.RequestHandler
 
 export class IpDeniedError extends Error {
-  status: number;
-  constructor(
-    message: string,
-    extra: any,
-  );
+  status: number
+  constructor(message: string, extra: any)
 }
